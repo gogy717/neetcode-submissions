e9395a72f77ccc640464bc6883e4b6c5e7cbe009
gogy717 <108651132+gogy717@users.noreply.github.com>
2026-08-19 13:44:58 +1000

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.java
@@ -0,0 +1,30 @@
+class LRUCache {
+    private Map<Integer, Integer> map = new HashMap<>();
+    private Queue<Integer> queue = new LinkedList<>();
+    private int cacheSize;
+    public LRUCache(int capacity) {
+        cacheSize = capacity;
+    }
+    
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            queue.remove(key);
+            queue.offer(key);
+        }
+        return map.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            queue.remove(key);
+        }
+        
+        queue.offer(key);
+        map.put(key, value);
+
+        if (queue.size() > cacheSize) {
+            int removed = queue.poll();
+            map.remove(removed);
+        }
+    }
+}
